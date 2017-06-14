b9a35175a398bcccb48c3112a552052776d1323f
Rob Miller <Rob@Beccas-MBP.local>
2017-06-14 10:52:26 -0700

    remove hello

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Rooms from '@/components/Rooms'
 import Room from '@/components/Room'
 
@@ -8,11 +7,6 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
-    },
     {
       path: '/rooms',
       name: 'Rooms',
